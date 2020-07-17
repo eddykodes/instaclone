d1990f902e3433f83eb9cc6f8a8cd666b4338576
Eddy <eddy@gocollo.com>
2020-07-17 19:06:10 -0400

    add Form to EditProfile component

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -4,19 +4,43 @@ import { connect } from 'react-redux'
 // Bootstrap
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export class EditProfile extends Component {
   constructor(){
     super()
     this.state = {
-      show: false
+      show: false,
+      userName: '',
+      name: '',
+      bio: '',
+      errors: {}
     }
+  }
+  mapUserToState = (user) => {
+    console.log(user)
+    this.setState({
+      userName: user.userName ? user.userName : '',
+      name: user.name ? user.name : '',
+      bio: user.bio ? user.bio : '',
+    })
   }
   handleShow = () => {
     this.setState({ show: true })
+    this.mapUserToState(this.props.user.credentials)
   }
   handleClose = () => {
     this.setState({ show: false })
+  }
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
   }
   render() {
     return (
@@ -24,9 +48,41 @@ export class EditProfile extends Component {
         <Button size='sm' variant='outline-dark' onClick={this.handleShow}>Edit Profile</Button>
 
         <Modal show={this.state.show} onHide={this.handleClose}>
-          <div className='EditProfile'>
-            Edit profile form here
-          </div>
+          <Form className='EditProfile p-3' onSubmit={this.handleSubmit}>
+            <Form.Group as={Row} controlId="formUserName">
+              <Form.Label column sm={2}>
+                Username
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Control type='text' value={this.state.userName} onChange={this.handleChange} isInvalid={this.state.errors.userName ? (true):(false)} />
+                <Form.Control.Feedback type='invalid'>{this.state.errors.userName}</Form.Control.Feedback>
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} controlId="formName">
+              <Form.Label column sm={2}>
+                Name
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Control type='text' value={this.state.name} onChange={this.handleChange} isInvalid={this.state.errors.name ? (true):(false)} />
+                <Form.Control.Feedback type='invalid'>{this.state.errors.name}</Form.Control.Feedback>
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} controlId="formBio">
+              <Form.Label column sm={2}>
+                Bio
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Control type='text' value={this.state.bio} onChange={this.handleChange} isInvalid={this.state.errors.bio ? (true):(false)} />
+                <Form.Control.Feedback type='invalid'>{this.state.errors.bio}</Form.Control.Feedback>
+              </Col>
+            </Form.Group>
+            <div className='d-flex justify-content-around mt-3'>
+              <span className='link-unstyled' onClick={this.handleClose}><b>Cancel</b></span>
+              <span className='btn-link link-unstyled' onClick={this.handleSubmit}><b>Post</b></span>
+            </div>
+            
+          </Form>
+
         </Modal>
       </Fragment>
     )
@@ -34,11 +90,11 @@ export class EditProfile extends Component {
 }
 
 const mapStateToProps = (state) => ({
-  
+  user: state.user
 })
 
-const mapDispatchToProps = {
+const mapActionsToProps = {
   
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(EditProfile)
+export default connect(mapStateToProps, mapActionsToProps)(EditProfile)
