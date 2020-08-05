253b928dd11fde1bb9a4ac6c0ce83d11f5b7cfe7
Eddy <eddy@gocollo.com>
2020-08-05 18:13:13 -0400

    design user profiles in Followers component

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -5,6 +5,8 @@ import { connect } from 'react-redux'
 
 // Bootstrap
 import Modal from 'react-bootstrap/Modal'
+import Image from 'react-bootstrap/Image'
+import Button from 'react-bootstrap/Button'
 
 export class Followers extends Component {
   constructor(){
@@ -25,8 +27,21 @@ export class Followers extends Component {
         <li className="list-inline-item" onClick={this.handleShow}><b>{this.props.followerCount}</b> followers</li>
 
         <Modal show={this.state.show} onHide={this.handleClose}>
-          <div>
-            list of followers here
+          <div className='p-3'>
+
+            <div className='d-flex justify-content-between mb-2'>
+              <div className='home-profile d-flex align-items-center'>
+                <Image src={this.props.user.credentials.userImage} alt='profile' className='mr-3'/>
+                <ul className='list-unstyled m-0'>
+                  <li><b>{this.props.user.credentials.userName}</b></li>
+                  <li className='text-grey'>{this.props.user.credentials.userName}</li>
+                </ul>
+              </div>
+              <div className='d-flex align-items-center'>
+                <Button size='sm' variant='primary'>Follow</Button>
+              </div>
+            </div>
+
           </div>
         </Modal>
       </Fragment>
@@ -35,7 +50,7 @@ export class Followers extends Component {
 }
 
 const mapStateToProps = (state) => ({
-  
+  user: state.user
 })
 
 const mapActionsToProps = {
